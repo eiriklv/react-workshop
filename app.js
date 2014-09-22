5951ba920c65bba893489e5129eac35f894f03a6
Erik Wendel <erik@wendel.no>
2014-09-22 22:49:08 +0200

    added simple express.js file server

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 var express = require('express');
-var routes = require('./routes');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var errorhandler = require('errorhandler');
-var http = require('http');
+var app = express();
 var path = require('path');
 
-var app = express();
+// app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'tasks'))); //  "public" off of current is root
 
-try {
-	app.locals.build = require('./public/build/build-info.json');
-} catch (err) {
-	app.locals.build = { revision: new Date().toISOString() };
-}
-
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pejs');
-app.disable('x-powered-by');
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-
-app.get('/', routes.index);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// TODO: add error handler
-
-// development only
-if ('development' === app.get('env')) {
-	app.use(errorhandler());
-}
-
-http.createServer(app).listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(3000);
+console.log('Listening on port 3000');
