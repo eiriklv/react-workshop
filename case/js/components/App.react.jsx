67e199e073d7e8985ba53f3a6ae52f544593cce5
Erik Wendel <erik@wendel.no>
2014-10-26 20:45:53 +0100

    added map

--- case/js/components/App.react.jsx
+++ case/js/components/App.react.jsx
@@ -10,7 +10,7 @@ module.exports = React.createClass({
 
     getInitialState: function() {
         return {
-            zoom: 10
+            zoom: 5
         }
     },
 
@@ -25,24 +25,35 @@ module.exports = React.createClass({
     },
 
     render: function() {
+
         return <div className="container">
-        			<h1>Hello Maps</h1>
-        			<Map
-        				height={500}
+        			<Map className="test"
+                        width="100%"
+                        height="100%"
 	        			zoom={ this.state.zoom }
+                        scaleControl={false}
+                        streetViewControl={false}
+                        mapTypeId={window.google.maps.MapTypeId.ROADMAP}
+                        panControl={false}
+                        zoomControl={false}
+                        mapTypeControl={false}
 	        			onZoomChange={this.zoomChanged}
 					    initialCenter={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} >
 
-					    <Marker
-					      onClick={this.handleClick}
-					      position={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} />
+                    <Marker
+                      onClick={this.handleClick}
+                      position={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} />
 
-					    <OverlayView
-					      style={{backgroundColor: 'gray'}}
-					      position={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} >
-					      <p>My state: {this.state.zoom}</p>
-					    </OverlayView>
-					  </Map>,
+                    <Marker
+                    onClick={this.handleClick}
+                    position={new GoogleMapsAPI.LatLng(37.774929500000000000, -122.419415500000010000)} />
+
+                    <OverlayView
+                      style={{backgroundColor: 'gray'}}
+                      position={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} >
+                      <p>My state: {this.state.zoom}</p>
+                    </OverlayView>
+                    </Map>
         		</div>
     }
 
