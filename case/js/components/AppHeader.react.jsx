3b9ec47acd7a9f6dfd492e1ce6fd2806498e24a8
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-10-28 00:20:09 +0100

    less tabs

--- case/js/components/AppHeader.react.jsx
+++ case/js/components/AppHeader.react.jsx
@@ -8,15 +8,15 @@ var AppHeader = React.createClass({
 
     render: function () {
         return <div className="app-header">
-        	<h1>Twitterizer</h1>
-        	<div>
-        		<span className="tweet-stats-desc">seconds running</span>
-        		<strong><Timer/></strong>
-        	</div>
-        	<div>
-        		<span className="tweet-stats-desc">tweets captured</span>
-        		<strong>{this.props.tweetCount}</strong>
-        	</div>
+            <h1>Twitterizer</h1>
+            <div>
+                <span className="tweet-stats-desc">seconds running</span>
+                <strong><Timer/></strong>
+            </div>
+            <div>
+                <span className="tweet-stats-desc">tweets captured</span>
+                <strong>{this.props.tweetCount}</strong>
+            </div>
         </div>;
     }
 });
