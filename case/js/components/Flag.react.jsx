35ca694476ba0c13257b73e4003499efc0ed2d34
Erik Wendel <erik@wendel.no>
2014-10-28 00:47:06 +0100

    added Flag component

---
+++ case/js/components/Flag.react.jsx
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Flag = React.createClass({
+    displayName: 'Flag',
+
+    render: function () {
+
+    	var countryCode = this.props.countryCode.toLowerCase();
+        return <span className={"tweet-flag flag-icon flag-icon-" + countryCode + " flag-icon-squared"}></span>;
+    }
+});
+
+module.exports = Flag;
