62cdb715e686a24f0b3f47c15494675746abfce5
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-10-27 20:02:33 +0100

    use findwhere instead of where

--- case/js/components/App.react.jsx
+++ case/js/components/App.react.jsx
@@ -4,8 +4,6 @@ var _ = require('underscore');
 
 var ws = new WebSocket('ws://localhost:9999');
 
-
-
 module.exports = React.createClass({
 
     componentDidMount: function () {
@@ -13,7 +11,7 @@ module.exports = React.createClass({
             ms = JSON.parse(ms.data);
             var last = this.state.tweets.concat([ms]).slice(-100);
             this.setState({ tweets: last });
-        }.bind(this); 
+        }.bind(this);
     },
 
     getInitialState: function() {
@@ -23,7 +21,7 @@ module.exports = React.createClass({
     },
 
     showTweet: function(id) {
-        var tweet = _(this.state.tweets).chain().where({ id: id }).first().value();
+        var tweet = _.findWhere(this.state.tweets, { id: id });
 
         if (!tweet) console.log('Tweet no longer in last hundred');
 
@@ -33,10 +31,9 @@ module.exports = React.createClass({
     render: function() {
 
         return <div>
-                <TweetMap 
-                    tweets={ this.state.tweets } 
-                    showTweet={ this.showTweet} />
-
+            <TweetMap
+                tweets={ this.state.tweets }
+                showTweet={ this.showTweet} />
         </div>;
     }
 
--- case/js/components/TweetMap.react.jsx
+++ case/js/components/TweetMap.react.jsx
@@ -14,37 +14,37 @@ var TweetMap = React.createClass({
 
     render: function() {
 
-    	var props = this.props;
+        var props = this.props;
 
         var markers = this.props.tweets.map(function(t) {
-        	var callback = function() {
-        		props.showTweet(t.id);
-        	}
+            var callback = function() {
+                props.showTweet(t.id);
+            }
 
             return <Marker
-                      onClick={ callback }
-                      position={new GoogleMapsAPI.LatLng(t.geo.coordinates[1], t.geo.coordinates[0])} />
+                onClick={ callback }
+                position={new GoogleMapsAPI.LatLng(t.geo.coordinates[1], t.geo.coordinates[0])} />
         });
 
         return <div className="container">
-        			<Map className="test"
+                    <Map className="test"
                         width="100%"
                         height="100%"
-	        			initialZoom={3}
+                        initialZoom={3}
                         scaleControl={false}
                         streetViewControl={false}
                         panControl={false}
                         zoomControl={false}
                         mapTypeControl={false}
-					    initialCenter={new GoogleMapsAPI.LatLng(30.675226, -35.051272)} >
-                
+                        initialCenter={new GoogleMapsAPI.LatLng(30.675226, -35.051272)} >
+
                     { markers }
                     <OverlayView
                       style={{backgroundColor: 'gray'}}
                       position={new GoogleMapsAPI.LatLng(59.9191310, 10.7585240)} >
                     </OverlayView>
                     </Map>
-        		</div>
+                </div>
     }
 });
 
