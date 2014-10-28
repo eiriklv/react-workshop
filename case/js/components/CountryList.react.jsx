8906b8b19580ed4d19352fad8a0e0c4dfc37a4ea
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-10-28 14:18:20 +0100

    oh yeah!

--- case/js/components/CountryList.react.jsx
+++ case/js/components/CountryList.react.jsx
@@ -1,5 +1,6 @@
 /*** @jsx React.DOM */
 var React = require('react');
+var _ = require('lodash');
 
 var countries = require('../util/countries');
 var Flag = require('./Flag.react');
@@ -9,22 +10,24 @@ var NO_OF_COUNTRIES = 25;
 var CountryList = React.createClass({
 
     render: function() {
-        var list = [];
-        for (var prop in this.props.countries) {
-            list.push({ countryCode: prop, tweetCount: this.props.countries[prop] })
-        }
-        list.sort(function(x,y) {
-            return x.tweetCount < y.tweetCount ? 1 : -1 }
-        );
-        list = list.slice(0, NO_OF_COUNTRIES);
+        var items = _.chain(this.props.countries)
+            .map(function(count, country) {
+                return { countryCode: country, tweetCount: count };
+            })
+            .sortBy(function(item) {
+                return -item.tweetCount;
+            })
+            .slice(0, NO_OF_COUNTRIES)
+            .map(function(item, index) {
+                return <li key={item.countryCode}>
+                           <Flag countryCode={item.countryCode} />
+                           <span className="country-tweet-count">{ item.tweetCount }</span>
+                       </li>
+            })
+            .value();
 
         return <ul className="countrylist">
-            { list.map(function(item, index) {
-                return <li key={item.countryCode}>
-                        <Flag countryCode={item.countryCode} />
-                        <span className="country-tweet-count">{ item.tweetCount }</span>
-                        </li>
-            })}
+            { items }
         </ul>;
     }
 
