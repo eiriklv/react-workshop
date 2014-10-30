30fdf9d8cc4211de59c879dcd53f2bec5c9935e4
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-10-30 20:18:45 +0100

    routes

--- case/js/components/App.react.jsx
+++ case/js/components/App.react.jsx
@@ -14,13 +14,24 @@ module.exports = React.createClass({
     },
 
     componentDidMount: function () {
+        // Here we've just set up the basics for one way of handling routes in
+        // a React applications. Basically, we treat a route as application
+        // state instead of something that lays on the outside of your
+        // application.
+
+        // However, as soon as we start to handle routes, we see that we need
+        // to move the state from our Dashboard component and into this
+        // component.  Better yet, we should move it out of the React
+        // components entirely. One way of handling this is by following the
+        // architectural principles called Flux (https://facebook.github.io/flux/).
+        // Another way could be to use Omniscient (https://github.com/omniscientjs/omniscient).
+        // Or just to put it into pure JavaScript objects.
+
         page('/', function() {
-            console.log('ROOT');
             this.setState({ route: 'root' });
         }.bind(this));
 
         page('/starred', function() {
-            console.log('STARRED');
             this.setState({ route: 'starred' });
         }.bind(this));
 
